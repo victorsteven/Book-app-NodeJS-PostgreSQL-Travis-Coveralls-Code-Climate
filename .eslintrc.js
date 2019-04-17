f9adc9ef870541319a672886ca0a51b10dbdf3e0
steven victor <chikodi543@gmail.com>
2019-04-17 23:18:48 +0100

    Setup babel to transpile ES6 code to ES5

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    "extends": "airbnb-base",
+
+    "rules": {
+        "no-console": 0,
+        "no-param-reassign": [2, {"props": false}],
+        "prefer-destructuring": 0,
+        "treatUndefinedAsUnspecified": true,
+        "arrow-body-style": 0,
+        "comma-dangle": 0,
+      },
+};
--- index.js
+++ index.js
@@ -3,3 +3,18 @@ import bodyParser from 'body-parser';
 
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// This is the default route that will display.
+app.get('*', (req, res) => res.status(200).send({
+  message: 'Welcome to this API.',
+}));
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server is running on PORT ${port}`);
+});
+
+export default app;
